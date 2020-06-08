515d88af274c3e89c975f93a7796fe54aab0bea1
kharioki <antolisah@gmail.com>
2020-06-08 08:02:19 +0300

    bottom nav bar with rounded corners

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kiatu_store/core/const.dart';
 import 'package:kiatu_store/core/flutter_icons.dart';
+import 'package:kiatu_store/models/shoe_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ShoeModel> shoeList = ShoeModel.list;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,7 +69,116 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
+          SizedBox(height: 24),
+          ...shoeList.map(
+            (data) => Container(
+              margin: EdgeInsets.only(left: 16, right: 16, bottom: 10),
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('assets/images/${data.imgPath}'),
+                    width: 100,
+                    height: 60,
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width * .4,
+                          child: Text(
+                            '${data.name}',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '${data.brand}',
+                          style: TextStyle(
+                            color: Colors.black26,
+                            height: 1.5,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Text(
+                      '\$${data.price.toInt()}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 1,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: AppColors.greenColor,
+          unselectedItemColor: Colors.black26,
+          currentIndex: 1,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(FlutterIcons.compass),
+              title: Text('data'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterIcons.list),
+              title: Text('data'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterIcons.bag),
+              title: Text('data'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterIcons.person_outline),
+              title: Text('data'),
+            ),
+          ],
+        ),
       ),
     );
   }
