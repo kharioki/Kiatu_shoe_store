757ddacd3f2e56bb2184ba08a468ba748d9c4197
kharioki <antolisah@gmail.com>
2020-06-08 12:38:54 +0300

    detail page conditional clip path

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -4,6 +4,7 @@ import 'package:kiatu_store/core/const.dart';
 import 'package:kiatu_store/core/flutter_icons.dart';
 import 'package:kiatu_store/models/shoe_model.dart';
 import 'package:kiatu_store/widgets/app_clipper.dart';
+import 'dart:math' as math;
 
 class DetailPage extends StatefulWidget {
   final ShoeModel shoeModel;
@@ -25,8 +26,13 @@ class _DetailPageState extends State<DetailPage> {
         title: Text(
           'CATEGORIES',
         ),
-        leading: Icon(
-          FlutterIcons.left_open_1,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            FlutterIcons.left_open_1,
+          ),
         ),
       ),
       body: Container(
@@ -37,10 +43,13 @@ class _DetailPageState extends State<DetailPage> {
             Positioned(
               bottom: 0,
               child: Container(
-                height: MediaQuery.of(context).size.height * .8,
+                height: MediaQuery.of(context).size.height * .75,
                 width: MediaQuery.of(context).size.width,
                 child: ClipPath(
-                  clipper: AppClipper(cornerSize: 50, diagonalHeight: 180),
+                  clipper: AppClipper(
+                      cornerSize: 50,
+                      diagonalHeight: 180,
+                      roundedBottom: false),
                   child: Container(
                     color: Colors.white,
                     padding: EdgeInsets.only(top: 180, left: 16, right: 16),
@@ -82,17 +91,13 @@ class _DetailPageState extends State<DetailPage> {
                             fontSize: 18,
                           ),
                         ),
+                        SizedBox(height: 16),
                         Row(
                           children: <Widget>[
-                            Container(
-                              height: 20,
-                              width: 20,
-                              decoration: BoxDecoration(
-                                color: AppColors.blueColor,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(50)),
-                              ),
-                            ),
+                            _buildColorOption(AppColors.blueColor),
+                            _buildColorOption(AppColors.greenColor),
+                            _buildColorOption(AppColors.orangeColor),
+                            _buildColorOption(AppColors.redColor),
                           ],
                         ),
                       ],
@@ -101,8 +106,95 @@ class _DetailPageState extends State<DetailPage> {
                 ),
               ),
             ),
+            Positioned(
+              bottom: 0,
+              child: _buildBottom(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Transform.rotate(
+                angle: -math.pi / 7,
+                child: Image(
+                  width: MediaQuery.of(context).size.width * .85,
+                  image:
+                      AssetImage('assets/images/${widget.shoeModel.imgPath}'),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildBottom() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 1,
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'PRICE',
+                style: TextStyle(
+                  color: Colors.black26,
+                ),
+              ),
+              Text(
+                '\$${widget.shoeModel.price.toInt()}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+            decoration: BoxDecoration(
+              color: AppColors.greenColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            child: Text(
+              'ADD TO CART',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildColorOption(Color color) {
+    return Container(
+      height: 20,
+      width: 20,
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(50)),
       ),
     );
   }
